feff1dac4c11512dff5a290da788f82e3a2705fc
aliss <gribova@spasskievorota.com>
2025-03-26 18:52:09 +0300

    fix assets

--- components/assets.php
+++ components/assets.php
@@ -1,5 +1,7 @@
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Пользовательские -->
+<link href="/assets/site.css" rel="stylesheet">
 <!-- Font Awesome для иконок -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 <!-- Шрифты -->
@@ -9,7 +11,6 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Пользовательские -->
-<link href="/assets/site.css" rel="stylesheet">
 <script src="/assets/site.js"></script>
 
 <svg style="visibility: hidden; position: absolute;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
