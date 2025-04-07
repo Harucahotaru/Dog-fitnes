17faeb8fd90e502a648cfb21065078beabeb81b2
aliss <gribova@spasskievorota.com>
2025-04-07 10:35:29 +0300

    fix navbar z-index

--- components/header.php
+++ components/header.php
@@ -9,7 +9,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse" id="navbarNav" style="z-index: 100;">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
                     <a class="nav-link text-dark" aria-current="page" href="/">Главная</a>
